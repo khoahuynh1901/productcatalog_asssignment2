826931cc29249b427cfacc1d7fb12d7ec433690d
Khoa Huynh <khoahuynh1901@gmail.com>
2025-03-27 09:25:16 -0400

    add style for content view

--- ProductCatalog/ContentView.swift
+++ ProductCatalog/ContentView.swift
@@ -3,8 +3,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            ProductListView()
+            VStack {
+                ProductListView()
+            }
+            .navigationBarTitle("Product Catalog", displayMode: .inline)
+            .navigationBarItems(trailing: NavigationLink(destination: AddProductView()) {
+                Text("Add Product")
+                    .font(.body)
+                    .foregroundColor(.blue)
+                    .padding(8)
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .shadow(radius: 3)
+            })
         }
+        .accentColor(.blue) // Customizes the Navigation bar color
+        .background(Color(.systemGray6).edgesIgnoringSafeArea(.all)) // Background for the entire screen
     }
 }
 
